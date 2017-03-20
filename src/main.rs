405a244930f6d93373fd1a251e5d10e779320089
Lukas Reuter <lukas@bootsmann-games.de>
2017-03-20 20:05:00 +0100

    added safe zone rendering

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@ extern crate qrcode;
 extern crate termion;
 extern crate image;
 
-use clap::{App, Arg, SubCommand};
-use qrcode::QrCode;
+use clap::{App, Arg};
+use qrcode::{QrCode, EcLevel};
 use termion::color;
+use termion::style;
 use image::GrayImage;
 
+/*TODO: add arguments for:
+- safe zone rendering
+- ec level
+- size of qr code
+- implement different types of qr payloads
+*/
 fn main() {
     let matches = App::new("qrterm")
         .version("0.1")
@@ -27,32 +34,48 @@ fn main() {
 
     let input = matches.value_of("INPUT").unwrap();
 
-    //println!("Converting input string: {}", &input);
+    let code = QrCode::with_error_correction_level(input, EcLevel::H).unwrap();
 
-    let code = QrCode::new(input).unwrap();
-
-    //let image: GrayImage = code.render().to_image();
-    //image.save("/tmp/qrcode.png").unwrap();
+    let image: GrayImage = code.render().to_image();
+    image.save("/tmp/qrcode.png").unwrap();
 
     let bit_array = code.to_vec();
 
-    let size = bit_array.len();
     let w = code.width();
+    let wide = w + 6;
 
-    for i in 0..size {
-        let item = bit_array[i];
+    //Draw the first white safe zone
+    for a in 1..(wide * 3) + 1 {
+        print!("{}  ", color::Bg(color::LightWhite));
+        if a % wide == 0 {
+            println!("{}", color::Bg(color::Reset));
+        }
+    }
 
-        if item {
-            print!("{}{}██", color::Fg(color::Black), color::Bg(color::Black)); //▜
+    for (i, item) in bit_array.iter().enumerate() {
+        //left safe zone
+        if i % w == 0 {
+            print!("{}      ", color::Bg(color::LightWhite));
+        }
+
+        if *item {
+            print!("{}  ", color::Bg(color::Black)); //█ ▜
         } else {
-            print!("{}{}██", color::Fg(color::LightWhite), color::Bg(color::LightWhite));
+            print!("{}  ", color::Bg(color::LightWhite));
         }
 
         if (i + 1) % w == 0 {
-            println!("{}{}", color::Fg(color::Reset), color::Bg(color::Reset));
+            println!("{}      {}",
+                     color::Bg(color::LightWhite),
+                     color::Bg(color::Reset));
         }
     }
-    //println!("");
 
-    //println!("Hello, world!");
+    //Draw the last white safe zone
+    for a in 1..(wide * 3) + 1 {
+        print!("{}  ", color::Bg(color::LightWhite));
+        if a % wide == 0 {
+            println!("{}", color::Bg(color::Reset));
+        }
+    }
 }
